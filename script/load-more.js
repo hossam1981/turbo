1c22c2d2ee0410066c42d3737ae4f17cdd624ace
Hossam Elnaggar <[hossam.elnaggar.ny@gmail.com]>
2022-06-17 14:30:32 -0400

    add fleet infinit

--- script/load-more.js
+++ script/load-more.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){  // <<--do i actually need this or not 
+$(document).ready(function () {  // <<--do i actually need this or not 
 
-// console.log('jquery magic is ready')
+    // console.log('jquery magic is ready')
 
-$(function(){
-    $(".box-hidden").slice(0 ,2).show();
+    $(function () {
+        $(".box-hidden").slice(0, 1).show();
 
-    $("#loadMore").on('click', function(e){
-        e.preventDefault();
-        // console.log("heloooo function")
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
+            // console.log("heloooo function")
 
-        $(".box-hidden:hidden").slice(0 , 2).slideDown();
-        if ($(".box-hidden:hidden").length == 0) {
-            $("#load").fadeOut('slow');
-        }
-       
-        $('html,body').animate({
-            scrollTop: $(this).offset().top 
-        }, 1500);
-        //  console.log("i'm a here sir")
-      }) 
+            $(".box-hidden:hidden").slice(0, 2).slideDown();
+            if ($(".box-hidden:hidden").length == 0) {
+                $("#load").fadeOut('slow');
+            }
+
+            $('html,body').animate({
+                scrollTop: $(this).offset().top
+            }, 1500);
+            //  console.log("i'm a here sir")
+        })
 
     })
 
@@ -28,23 +28,23 @@ $(function(){
 
 
 // -----function to check if the js file woring or exist---
-    function checkIfIncluded(file) {
-        var links = document.getElementsByTagName("link");
-        for(var i = 0; i < links.length; i++) {
-            if (links[i].href.substr(-file.length) == file)
-                return true;
-        }
-    
-        var scripts = document.getElementsByTagName("script");
-        for(var i = 0; i < scripts.length; i++) {
-            if (scripts[i].src.substr(-file.length) == file)
-                return true;
-        }
-    
-        return false;
+function checkIfIncluded(file) {
+    var links = document.getElementsByTagName("link");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].href.substr(-file.length) == file)
+            return true;
     }
-    
-    // console.log(checkIfIncluded("mystyle.css"));
-    console.log(checkIfIncluded("script.js"));
-    console.log(checkIfIncluded("load-more.js"));
+
+    var scripts = document.getElementsByTagName("script");
+    for (var i = 0; i < scripts.length; i++) {
+        if (scripts[i].src.substr(-file.length) == file)
+            return true;
+    }
+
+    return false;
+}
+
+// console.log(checkIfIncluded("mystyle.css"));
+console.log(checkIfIncluded("script.js"));
+console.log(checkIfIncluded("load-more.js"));
 //
